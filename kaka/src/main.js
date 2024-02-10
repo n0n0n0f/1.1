18c3b11210f7ee10f7f8e18b967a68151fb30099
nathan <r4sl3xgod@gmail.com>
2024-02-10 19:52:28 +0700

    Добавили кнопки добавления и изменили выбор цвета

--- kaka/src/main.js
+++ kaka/src/main.js
@@ -6,22 +6,36 @@ let app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
+        inStock: false,
         inventory: 100,
         onSale: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        },
+        ReduceToCart() {
+            if (this.cart >= 1){
+                this.cart -= 1
+            }
+        }
     }
-})
-
+});
