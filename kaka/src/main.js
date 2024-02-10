9134afbdb254d90cb680ba0fada98d98dc0acae1
nathan <r4sl3xgod@gmail.com>
2024-02-11 00:39:37 +0700

    Сделал отзывы

--- kaka/src/main.js
+++ kaka/src/main.js
@@ -127,27 +127,127 @@ Vue.component('product-details', {
 `
 });
 
+Vue.component('product-review', {
+    template: `
+      <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+          <li v-for="review in reviews" :key="review.id">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+
+        <form class="review-form" @submit.prevent="onSubmit">
+          <div v-if="errors.length">
+            <b>Please correct the following error(s):</b>
+            <ul>
+              <li v-for="error in errors">{{ error }}</li>
+            </ul>
+          </div>
+          <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name" placeholder="name">
+          </p>
+          <p>
+            <label for="name">Name:</label>
+            <input id="name" v-model="name" placeholder="name">
+          </p>
+
+          <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+          </p>
+
+          <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+              <option>5</option>
+              <option>4</option>
+              <option>3</option>
+              <option>2</option>
+              <option>1</option>
+            </select>
+          </p>
+
+          <p>Would you recommend this product?</p>
+          <label>
+            Yes
+            <input type="radio" value="Yes" v-model="recommend"/>
+          </label>
+          <label>
+            No
+            <input type="radio" value="No" v-model="recommend"/>
+          </label>
+
+          <p>
+            <input type="submit" value="Submit">
+          </p>
+        </form>
+      </div>
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null,
+            recommend: null,
+            reviews: [],
+            errors: []
+        }
+    },
+    methods:{
+        onSubmit() {
+            this.errors = [];
+            if(this.name && this.review && this.rating && this.recommend) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend,
+                    id: this.reviews.length + 1 // Add unique id for each review
+                };
+                this.$emit('review-submitted', productReview);
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+                this.recommend = null;
+            } else {
+                if(!this.name) this.errors.push("Name required.");
+                if(!this.review) this.errors.push("Review required.");
+                if(!this.rating) this.errors.push("Rating required.");
+                if(!this.recommend) this.errors.push("Recommendation required.");
+            }
+        }
+    }
+});
+
+
 let app = new Vue({
     el: '#app',
     data: {
         premium: true,
-        cart: []
+        cart: [],
+        reviews: []
     },
     methods: {
         updateCart(id) {
-            this.cart.push(id) ;
+            this.cart.push(id);
         },
-
         updateCartminus(id) {
             const index = this.cart.indexOf(id);
-            if (index !== -1){
+            if (index !== -1) {
                 this.cart.splice(index, 1);
             }
-            }
         },
+        addReview(productReview) {
+            this.reviews.push(productReview);
+        }
+    }
+});
 
-
-})
 
 
 
