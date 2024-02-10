33ce750025bfeb3c9ed8b1dcfdd558fd754adfa3
nathan <r4sl3xgod@gmail.com>
2024-02-10 17:47:02 +0700

    Вывел картинку носочков на страницу

--- kaka/src/main.js
+++ kaka/src/main.js
@@ -1,6 +1,9 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks",
+        image: "./assets/vmSocks-green-onWhite.jpg"
     }
 })
+
