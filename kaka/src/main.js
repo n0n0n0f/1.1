a04143056b74527bdda80c5c392461a643722722
nathan <r4sl3xgod@gmail.com>
2024-02-10 18:35:12 +0700

    Сделал наличие товара и практическую 3

--- kaka/src/main.js
+++ kaka/src/main.js
@@ -7,7 +7,8 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-
+        inventory: 100,
+        onSale: true,
     }
 })
 
