ea749cb33a480dfe37b961612c42784e04dcc6f9
nathan <r4sl3xgod@gmail.com>
2024-02-10 21:04:14 +0700

    Сделал Практическое задание 7 и допы к нему

--- kaka/src/main.js
+++ kaka/src/main.js
@@ -2,13 +2,14 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
-        inventory: 100,
-        onSale: false,
+        inStock: true,
+        inventory: 0,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -21,21 +22,36 @@ let app = new Vue({
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        cart: 0,
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1;
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage;
-        },
-        ReduceToCart() {
-            if (this.cart >= 1){
-                this.cart -= 1
-            }
-        }
-    }
+            ],
+                sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+                cart: 0,
+                },
+                methods: {
+                    addToCart() {
+                        this.cart += 1;
+                    },
+                    updateProduct(variantImage) {
+                        this.image = variantImage;
+                    },
+                    ReduceToCart() {
+                        if (this.cart >= 1){
+                            this.cart -= 1
+                        }
+                    }
+
+                },
+                computed: {
+                    title() {
+                        return this.brand + ' ' + this.product;
+                    },
+                    sale() {
+                        if (this.onSale){
+                            return this.title + ' is on sale!'
+                        } else {
+                            return this.title + ' is not sale!'
+                        }
+                    }
+                },
+
+
 });
